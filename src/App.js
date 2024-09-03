1cb778ebeb51cb1ecdaf32ecd796c2cbface5cfd
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-09-02 23:26:23 -0400

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
 export default function App() {
-  return <div>Hello Adam</div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Where in the world?</h1>
+      <div className="theme-switcher">
+        <button
+          className="theme-switcher__button"
+          aria-label="Switch color theme"
+        >
+          <span className="theme-switcher__icon"></span>
+          <span className="theme-switcher__label">Dark Mode</span>
+        </button>
+      </div>
+    </header>
+  );
 }
