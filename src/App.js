94c89e87b83cb4df259b7e7de9d5d20c31d9c067
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-10-07 23:59:08 -0400

    Created details__back-btn-container element

--- src/App.js
+++ src/App.js
@@ -576,39 +576,41 @@ function CountryDetails({
 
   return (
     <div className="details">
-      <button
-        className={`details__back-btn ${
-          !isLightTheme ? "dark-slate-grey-bg" : ""
-        }`}
-        type="button"
-        onClick={onBackBtnClick}
-      >
-        <svg
-          className="details__back-arrow-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          viewBox="0 0 18 18"
-          fill="none"
-        >
-          <path
-            className={`details__back-arrow-icon-path ${
-              !isLightTheme ? "white-fill" : ""
-            }`}
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z"
-            fill="#111517"
-          />
-        </svg>
-        <span
-          className={`details__back-btn-label ${
-            !isLightTheme ? "white-color" : ""
+      <div className="details__back-btn-container">
+        <button
+          className={`details__back-btn ${
+            !isLightTheme ? "dark-slate-grey-bg" : ""
           }`}
+          type="button"
+          onClick={onBackBtnClick}
         >
-          Back
-        </span>
-      </button>
+          <svg
+            className="details__back-arrow-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+          >
+            <path
+              className={`details__back-arrow-icon-path ${
+                !isLightTheme ? "white-fill" : ""
+              }`}
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M5.81802 3.6967L6.87868 4.75736L3.3785 8.25754H16.7428L16.7428 9.74246H3.3785L6.87868 13.2426L5.81802 14.3033L0.514719 9L5.81802 3.6967Z"
+              fill="#111517"
+            />
+          </svg>
+          <span
+            className={`details__back-btn-label ${
+              !isLightTheme ? "white-color" : ""
+            }`}
+          >
+            Back
+          </span>
+        </button>
+      </div>
       <div className="details__flag-overview">
         <img
           className="details__flag"
