d0459b7533ad5b0dad9742ad28032010bd4f0754
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-10-12 23:02:46 -0400

    Created details__container element

--- src/App.js
+++ src/App.js
@@ -576,7 +576,7 @@ function CountryDetails({
 
   return (
     <div className="details">
-      <div className="details__back-btn-container">
+      <div className="details__container">
         <button
           className={`details__back-btn ${
             !isLightTheme ? "dark-slate-grey-bg" : ""
@@ -610,77 +610,79 @@ function CountryDetails({
             Back
           </span>
         </button>
-      </div>
-      <div className="details__flag-overview">
-        <img
-          className="details__flag"
-          src={flag}
-          alt={name === "N/A" ? name : `${name} flag`}
-        />
-        <div className="details__overview-border">
-          <div className="details__overview-container">
-            <div className="details__name-overview">
-              <span
-                className={`details__name ${
-                  !isLightTheme ? "white-color" : ""
-                }`}
-              >
-                {name}
-              </span>
-              <div className="details__overview">
-                <LabelValue label="Native Name" value={nativeName} />
-                <LabelValue label="Population" value={formattedPopulation} />
-                <LabelValue label="Region" value={region} />
-                <LabelValue label="Sub Region" value={subregion} />
-                <LabelValue label="Capital" value={capital} />
-              </div>
-            </div>
-            <div className="details__overview">
-              <LabelValue label="Top Level Domain" value={tld} />
-              <LabelValue
-                label="Currencies"
-                value={currencies === "N/A" ? currencies : formattedCurrencies}
-              />
-              <LabelValue
-                label="Languages"
-                value={languages === "N/A" ? languages : formattedLanguages}
-              />
-            </div>
-          </div>
-          <div className="border-countries">
-            {hasBorders ? (
-              <>
+        <div className="details__flag-overview">
+          <img
+            className="details__flag"
+            src={flag}
+            alt={name === "N/A" ? name : `${name} flag`}
+          />
+          <div className="details__overview-border">
+            <div className="details__overview-container">
+              <div className="details__name-overview">
                 <span
-                  className={`border-countries__label ${
+                  className={`details__name ${
                     !isLightTheme ? "white-color" : ""
                   }`}
                 >
-                  Border Countries:
+                  {name}
                 </span>
-                <ul className="border-countries__container">
-                  {formattedBorderCountries.map(
-                    (borderCountry) =>
-                      countriesData.current.some(
-                        (country) => country.name.common === borderCountry
-                      ) && (
-                        <li
-                          className={`border-countries__country ${
-                            !isLightTheme
-                              ? "dark-slate-grey-bg white-color box-shadow-dark"
-                              : ""
-                          }`}
-                          key={borderCountry}
-                          onClick={() => onBorderCountryClick(borderCountry)}
-                        >
-                          {borderCountry}
-                        </li>
-                      )
-                  )}
-                </ul>
-              </>
-            ) : (
-              <LabelValue label="Border Countries" value="N/A" />
-            )}
+                <div className="details__overview">
+                  <LabelValue label="Native Name" value={nativeName} />
+                  <LabelValue label="Population" value={formattedPopulation} />
+                  <LabelValue label="Region" value={region} />
+                  <LabelValue label="Sub Region" value={subregion} />
+                  <LabelValue label="Capital" value={capital} />
+                </div>
+              </div>
+              <div className="details__overview">
+                <LabelValue label="Top Level Domain" value={tld} />
+                <LabelValue
+                  label="Currencies"
+                  value={
+                    currencies === "N/A" ? currencies : formattedCurrencies
+                  }
+                />
+                <LabelValue
+                  label="Languages"
+                  value={languages === "N/A" ? languages : formattedLanguages}
+                />
+              </div>
+            </div>
+            <div className="border-countries">
+              {hasBorders ? (
+                <>
+                  <span
+                    className={`border-countries__label ${
+                      !isLightTheme ? "white-color" : ""
+                    }`}
+                  >
+                    Border Countries:
+                  </span>
+                  <ul className="border-countries__container">
+                    {formattedBorderCountries.map(
+                      (borderCountry) =>
+                        countriesData.current.some(
+                          (country) => country.name.common === borderCountry
+                        ) && (
+                          <li
+                            className={`border-countries__country ${
+                              !isLightTheme
+                                ? "dark-slate-grey-bg white-color box-shadow-dark"
+                                : ""
+                            }`}
+                            key={borderCountry}
+                            onClick={() => onBorderCountryClick(borderCountry)}
+                          >
+                            {borderCountry}
+                          </li>
+                        )
+                    )}
+                  </ul>
+                </>
+              ) : (
+                <LabelValue label="Border Countries" value="N/A" />
+              )}
+            </div>
           </div>
         </div>
       </div>
